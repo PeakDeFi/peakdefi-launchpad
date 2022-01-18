e42b186e440f98d175409097a66dd3b68fa836df
gladiypavlo@gmail.com <qwedsazxc@2000>
2022-01-18 21:30:41 +0200

    change name

--- PeakDefiSale.sol
+++ PeakDefiSale.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./ISalesFactory.sol";
 import "./IAllocationStaking.sol";
-contract DefiSale {
+contract PeakDefiSale {
     using SafeMath for uint256;
     IAllocationStaking public allocationStakingContract;
     ISalesFactory public factory;
